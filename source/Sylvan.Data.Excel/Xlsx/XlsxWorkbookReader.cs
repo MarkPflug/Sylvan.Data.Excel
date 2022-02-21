a1c9fe84ce516713faf454aa6294d4ac23414111
MarkPflug <MarkPflug@users.noreply.github.com>
2022-02-21 13:35:06 -0800

    Modify xlsx ReadString to allow empty string elements in rich text.

--- source/Sylvan.Data.Excel/Xlsx/XlsxWorkbookReader.cs
+++ source/Sylvan.Data.Excel/Xlsx/XlsxWorkbookReader.cs
@@ -959,15 +959,21 @@ sealed class XlsxWorkbookReader : ExcelDataReader
 				if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "t")
 				{
 					reader.Read();
-					if (reader.NodeType != XmlNodeType.Text && reader.NodeType != XmlNodeType.SignificantWhitespace)
+					string s = string.Empty;
+					if (reader.NodeType == XmlNodeType.Text || reader.NodeType == XmlNodeType.SignificantWhitespace)
 					{
-						if(reader.NodeType == XmlNodeType.EndElement)
-						{
-							return string.Empty;
-						}
+
+						s = reader.Value;
+					}
+					else if (reader.NodeType == XmlNodeType.EndElement)
+					{
+						// empty string.
+					}
+					else
+					{
 						throw new InvalidDataException();
 					}
-					var s = reader.Value;
+
 					if (c == 0)
 					{
 						str = s;
