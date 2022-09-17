35e37d644108cc5eba363b4a103805c2c1433cd6
MarkPflug <MarkPflug@users.noreply.github.com>
2022-09-17 09:50:39 -0700

    format files

--- source/MinimalXlsx/Program.cs
+++ source/MinimalXlsx/Program.cs
@@ -2,6 +2,8 @@
 using System.IO.Compression;
 
 var file = "minimal.xlsx";
+if (File.Exists(file))
+	File.Delete(file);
 ZipFile.CreateFromDirectory("Content", file);
 
 Process.Start(new ProcessStartInfo {  FileName = file, UseShellExecute = true});
