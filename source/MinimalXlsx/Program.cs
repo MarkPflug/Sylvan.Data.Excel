a8cd10a2645a033b4e808bad63f93dc2fa18ed34
MarkPflug <MarkPflug@users.noreply.github.com>
2022-09-17 09:48:12 -0700

    minimal xlsx

---
+++ source/MinimalXlsx/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+
+var file = "minimal.xlsx";
+ZipFile.CreateFromDirectory("Content", file);
+
+Process.Start(new ProcessStartInfo {  FileName = file, UseShellExecute = true});
