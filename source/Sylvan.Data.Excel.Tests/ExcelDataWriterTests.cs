f71d76ce0b5d2726f910efc5a1e795468ac3ccbb
MarkPflug <MarkPflug@users.noreply.github.com>
2022-09-24 11:29:04 -0700

    Add support for writing more data types.

--- source/Sylvan.Data.Excel.Tests/ExcelDataWriterTests.cs
+++ source/Sylvan.Data.Excel.Tests/ExcelDataWriterTests.cs
@@ -24,12 +24,12 @@ public class XlsxDataWriterTests : ExcelDataWriterTests
 
 public abstract class ExcelDataWriterTests
 {
-	protected abstract string GetFile([CallerMemberName] string name = "");
+	protected abstract string GetFile([CallerMemberName] string name = null);
 
 	public abstract ExcelWorkbookType WorkbookType { get; }
 	public object Enumable { get; private set; }
 
-	static void Unpack(string file, string folder)
+	static void Unpack(string file, [CallerMemberName] string folder = null)
 	{
 		// useful for debugging.
 		try
@@ -50,7 +50,7 @@ public abstract class ExcelDataWriterTests
 	}
 
 	[Fact]
-	public void TestCommonTypes()
+	public void CommonTypes()
 	{
 		// tests the most common types.
 		Random r = new Random();
@@ -74,12 +74,11 @@ public abstract class ExcelDataWriterTests
 		{
 			w.Write("data", reader);
 		}
-		Unpack(f, "dump");
 		Open(f);
 	}
 
 	[Fact]
-	public void WriteBigString()
+	public void BigString()
 	{
 		// this is the largest string that can be written.
 		// larger, and Excel will complain, and truncate it.
@@ -100,12 +99,11 @@ public abstract class ExcelDataWriterTests
 		{
 			w.Write("data", reader);
 		}
-		Unpack(f, "dump");
 		Open(f);
 	}
 
 	[Fact]
-	public void WriteNullCharString()
+	public void NullCharString()
 	{
 		var str = "a\0b";
 		var data =
@@ -127,7 +125,7 @@ public abstract class ExcelDataWriterTests
 	}
 
 	[Fact]
-	public void WriteBoolean()
+	public void Boolean()
 	{
 		var data =
 			Enumerable.Range(1, 100)
@@ -155,8 +153,158 @@ public abstract class ExcelDataWriterTests
 		var data = "a,b,c\n1,2,3\n1,2,3,4\n,1,2,3,4,5\n";
 		var r = new StringReader(data);
 		var csv = CsvDataReader.Create(r);
-		
+
 		var dr = csv.AsVariableField(c => c.RowFieldCount);
+		var f = GetFile();
+		using (var edw = ExcelDataWriter.Create(f))
+		{
+			edw.Write("data", dr);
+		}
+		Open(f);
+	}
+
+	[Fact]
+	public void Binary()
+	{
+		var data = new[]
+		{
+			new {
+				Name = "A",
+				Data = new byte[] {1,2,3,4 },
+			},
+			new {
+				Name = "B",
+				Data = new byte[] {1,2,3,4 },
+			}
+		};
+
+		var dr = data.AsDataReader();
+		var f = GetFile();
+		using (var edw = ExcelDataWriter.Create(f))
+		{
+			edw.Write("data", dr);
+		}
+		Open(f);
+	}
+
+	[Fact]
+	public void CharArray()
+	{
+		var data = new[]
+		{
+			new {
+				Name = "A",
+				Data = "Alphabet".ToCharArray(),
+			},
+			new {
+				Name = "B",
+				Data = new string('Z',short.MaxValue).ToCharArray(),
+			}
+		};
+
+		var dr = data.AsDataReader();
+		var f = GetFile();
+		using (var edw = ExcelDataWriter.Create(f))
+		{
+			edw.Write("data", dr);
+		}
+		Open(f);
+		Assert.True(false, "ObjectDataReader doesn't properly handle char[]. This ends up writing \"System.Char[]\", need to fix.");
+	}
+
+	[Fact]
+	public void Char()
+	{
+		var data = new[]
+		{
+			new {
+				Name = "Alpha",
+				Data = 'A',
+			},
+			new {
+				Name = "Beta",
+				Data = 'B',
+			}
+		};
+
+		var dr = data.AsDataReader();
+		var f = GetFile();
+		using (var edw = ExcelDataWriter.Create(f))
+		{
+			edw.Write("data", dr);
+		}
+		Open(f);
+	}
+
+	[Fact]
+	public void Byte()
+	{
+		var data = new[]
+		{
+			new {
+				Name = "Alpha",
+				Data = (byte)'A',
+			},
+			new {
+				Name = "Beta",
+				Data = (byte)'B',
+			}
+		};
+
+		var dr = data.AsDataReader();
+		var f = GetFile();
+		using (var edw = ExcelDataWriter.Create(f))
+		{
+			edw.Write("data", dr);
+		}
+		Open(f);
+	}
+
+	[Fact]
+	public void GuidData()
+	{
+		var data =
+			Enumerable.Range(0, 16)
+			.Select(
+				i =>
+				{
+					var b = new byte[16];
+					Array.Fill(b, (byte)(i | i << 4));
+					return new
+					{
+						Name = "Id " + i,
+						Guid = new Guid(b),
+						Value = i,
+					};
+				}
+			);
+
+		var dr = data.AsDataReader();
+		var f = GetFile();
+		using (var edw = ExcelDataWriter.Create(f))
+		{
+			edw.Write("data", dr);
+		}
+		Open(f);
+	}
+
+	[Fact]
+	public void TimeSpanData()
+	{
+		var data =
+			Enumerable.Range(0, 100)
+			.Select(
+				i =>
+				{
+					return new
+					{
+						TimeSpan = TimeSpan.FromSeconds(Math.PI * i),
+						Value = i,
+					};
+				}
+			);
+
+		var dr = data.AsDataReader();
 		var f = GetFile();
 		using (var edw = ExcelDataWriter.Create(f))
 		{
@@ -168,7 +316,7 @@ public abstract class ExcelDataWriterTests
 #if NET6_0_OR_GREATER
 
 	[Fact]
-	public void TestDateOnly()
+	public void DateOnly()
 	{
 		var data =
 			Enumerable.Range(1, 100)
@@ -177,6 +325,28 @@ public abstract class ExcelDataWriterTests
 				{
 					Id = i,
 					DateOnly = new DateOnly(2020, 1, 1).AddDays(i),
+				}
+			);
+
+		var f = GetFile();
+		var reader = data.AsDataReader();
+		using (var w = ExcelDataWriter.Create(f))
+		{
+			w.Write("data", reader);
+		}
+		Open(f);
+	}
+
+	[Fact]
+	public void TimeOnly()
+	{
+		var data =
+			Enumerable.Range(1, 100)
+			.Select(
+				i => new
+				{
+					Id = i,
+					TimeOnly = new TimeOnly(1, 0).AddMinutes(i * 7),
 				}
 			);
 
--- source/Sylvan.Data.Excel/Xlsx/XlsxDataWriter+FieldWriter.cs
+++ source/Sylvan.Data.Excel/Xlsx/XlsxDataWriter+FieldWriter.cs
@@ -20,12 +20,16 @@ partial class XlsxDataWriter
 		internal XlsxDataWriter dw;
 		internal TextWriter xw;
 		internal DbDataReader dr;
+		internal char[]? scratch;
+
+		public char[] GetScratch()
+		{
+			return scratch ?? (scratch = new char[64]);
+		}
 	}
 
 	abstract class FieldWriter
 	{
-		protected char[] scratch = new char[100];
-
 		public static FieldWriter Get(Type type)
 		{
 			var code = Type.GetTypeCode(type);
@@ -34,25 +38,56 @@ partial class XlsxDataWriter
 			{
 				case TypeCode.Boolean:
 					return new BooleanFieldWriter();
+				case TypeCode.Char:
+					return new CharFieldWriter();
 				case TypeCode.DateTime:
 					return new DateTimeFieldWriter();
 				case TypeCode.String:
 					return new StringFieldWriter();
+				case TypeCode.Byte:
+					return new ByteFieldWriter();
+				case TypeCode.Int16:
+					return new Int16FieldWriter();
 				case TypeCode.Int32:
 					return new Int32FieldWriter();
 				case TypeCode.Int64:
 					return new Int64FieldWriter();
+				case TypeCode.Single:
+					return new SingleFieldWriter();
 				case TypeCode.Double:
 					return new DoubleFieldWriter();
 				case TypeCode.Decimal:
 					return new DecimalFieldWriter();
 				default:
+					if (type == typeof(byte[]))
+					{
+						return new BinaryHexFieldWriter();
+					}
+					if (type == typeof(char[]))
+					{
+						return new CharArrayFieldWriter();
+					}
+					if (type == typeof(Guid))
+					{
+						return new GuidFieldWriter();
+					}
+					if (type == typeof(TimeSpan))
+					{
+						return new TimeSpanFieldWriter();
+					}
+
 #if DATE_ONLY
 					if (type == typeof(DateOnly))
 					{
 						return new DateOnlyFieldWriter();
 					}
+
+					if (type == typeof(TimeOnly))
+					{
+						return new TimeOnlyFieldWriter();
+					}
 #endif
+
 					return new ObjectFieldWriter();
 			}
 		}
@@ -117,6 +152,7 @@ partial class XlsxDataWriter
 			var dt = c.dr.GetDateTime(ordinal);
 			var val = (dt - Epoch).TotalDays + 2;
 #if SPAN
+			var scratch = c.GetScratch();
 			if (val.TryFormat(scratch.AsSpan(), out var sl))
 			{
 				w.Write(scratch, 0, sl);
@@ -138,9 +174,7 @@ partial class XlsxDataWriter
 
 	sealed class DateOnlyFieldWriter : FieldWriter
 	{
-
 		static readonly TimeOnly Midnight = new TimeOnly(0);
-
 
 		public override void WriteField(Context c, int ordinal)
 		{
@@ -150,6 +184,32 @@ partial class XlsxDataWriter
 			var dt = c.dr.GetFieldValue<DateOnly>(ordinal);
 			var val = (dt.ToDateTime(Midnight) - Epoch).TotalDays + 2;
 
+			var scratch = c.GetScratch();
+			if (val.TryFormat(scratch.AsSpan(), out var sl))
+			{
+				w.Write(scratch, 0, sl);
+			}
+
+			w.Write("</v></c>");
+		}
+
+		public override double GetWidth(DbDataReader data, int ordinal)
+		{
+			return 11;
+		}
+	}
+
+	sealed class TimeOnlyFieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			w.Write("<c s=\"3\"><v>");
+
+			var t = c.dr.GetFieldValue<TimeOnly>(ordinal);
+			var val = t.ToTimeSpan().TotalDays;
+
+			var scratch = c.GetScratch();
 			if (val.TryFormat(scratch.AsSpan(), out var sl))
 			{
 				w.Write(scratch, 0, sl);
@@ -175,6 +235,29 @@ partial class XlsxDataWriter
 
 			var val = c.dr.GetDecimal(ordinal);
 #if SPAN
+			var scratch = c.GetScratch();
+			if (val.TryFormat(scratch.AsSpan(), out var sl))
+			{
+				w.Write(scratch, 0, sl);
+			}
+#else
+			w.Write(val);
+#endif
+
+			w.Write("</v></c>");
+		}
+	}
+
+	sealed class SingleFieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			w.Write("<c><v>");
+
+			var val = c.dr.GetFloat(ordinal);
+#if SPAN
+			var scratch = c.GetScratch();
 			if (val.TryFormat(scratch.AsSpan(), out var sl))
 			{
 				w.Write(scratch, 0, sl);
@@ -196,6 +279,64 @@ partial class XlsxDataWriter
 
 			var val = c.dr.GetDouble(ordinal);
 #if SPAN
+			var scratch = c.GetScratch();
+			if (val.TryFormat(scratch.AsSpan(), out var sl))
+			{
+				w.Write(scratch, 0, sl);
+			}
+#else
+			w.Write(val);
+#endif
+
+			w.Write("</v></c>");
+		}
+	}
+
+	sealed class CharFieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			w.Write("<c t=\"str\"><v>");
+
+			var val = c.dr.GetChar(ordinal);
+			w.Write(val);
+			w.Write("</v></c>");
+		}
+	}
+
+	sealed class ByteFieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			w.Write("<c><v>");
+
+			var val = c.dr.GetByte(ordinal);
+#if SPAN
+			var scratch = c.GetScratch();
+			if (val.TryFormat(scratch.AsSpan(), out var sl))
+			{
+				w.Write(scratch, 0, sl);
+			}
+#else
+			w.Write(val);
+#endif
+
+			w.Write("</v></c>");
+		}
+	}
+
+	sealed class Int16FieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			w.Write("<c><v>");
+
+			var val = c.dr.GetInt16(ordinal);
+#if SPAN
+			var scratch = c.GetScratch();
 			if (val.TryFormat(scratch.AsSpan(), out var sl))
 			{
 				w.Write(scratch, 0, sl);
@@ -217,6 +358,7 @@ partial class XlsxDataWriter
 
 			var val = c.dr.GetInt32(ordinal);
 #if SPAN
+			var scratch = c.GetScratch();
 			if (val.TryFormat(scratch.AsSpan(), out var sl))
 			{
 				w.Write(scratch, 0, sl);
@@ -238,6 +380,7 @@ partial class XlsxDataWriter
 
 			var val = c.dr.GetInt64(ordinal);
 #if SPAN
+			var scratch = c.GetScratch();
 			if (val.TryFormat(scratch.AsSpan(), out var sl))
 			{
 				w.Write(scratch, 0, sl);
@@ -245,6 +388,126 @@ partial class XlsxDataWriter
 #else
 			w.Write(val);
 #endif
+
+			w.Write("</v></c>");
+		}
+	}
+
+	sealed class GuidFieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			// TODO: currently writing these as inline string.
+			// might make sense to put in shared string table instead.
+			w.Write("<c t=\"str\"><v>");
+
+			var val = c.dr.GetGuid(ordinal);
+#if SPAN
+			var scratch = c.GetScratch();
+			if (val.TryFormat(scratch.AsSpan(), out var sl))
+			{
+				w.Write(scratch, 0, sl);
+			}
+#else
+			w.Write(val);
+#endif
+
+			w.Write("</v></c>");
+		}
+
+		public override double GetWidth(DbDataReader data, int ordinal)
+		{
+			return 38;
+		}
+	}
+
+	sealed class TimeSpanFieldWriter : FieldWriter
+	{
+		public override void WriteField(Context c, int ordinal)
+		{
+			var w = c.xw;
+			// TODO: currently writing these as inline string.
+			// might make sense to put in shared string table instead.
+			w.Write("<c><v>");
+
+			var val = c.dr.GetFieldValue<TimeSpan>(ordinal).TotalSeconds;
+#if SPAN
+			var scratch = c.GetScratch();
+			if (val.TryFormat(scratch.AsSpan(), out var sl))
+			{
+				w.Write(scratch, 0, sl);
+			}
+#else
+			w.Write(val);
+#endif
+
+			w.Write("</v></c>");
+		}		
+	}
+
+	sealed class BinaryHexFieldWriter : FieldWriter
+	{
+		static char[] HexMap = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+		byte[] dataBuffer = new byte[48];
+
+		public override void WriteField(Context context, int ordinal)
+		{
+			var w = context.xw;
+			w.Write("<c t=\"str\"><v>");
+			var idx = 0;
+			var buffer = context.GetScratch();
+			int len;
+			var pos = 0;
+			var reader = context.dr;
+			w.Write("0x");
+			while ((len = (int)reader.GetBytes(ordinal, idx, dataBuffer, 0, dataBuffer.Length)) != 0)
+			{
+				var c = ToHexCharArray(dataBuffer, 0, len, buffer, pos);
+				w.Write(buffer, 0, c);
+				idx += len;
+				pos += c;
+			}
+
+			w.Write("</v></c>");
+		}
+
+		static int ToHexCharArray(byte[] dataBuffer, int offset, int length, char[] outputBuffer, int outputOffset)
+		{
+			if (length * 2 > outputBuffer.Length - outputOffset)
+				throw new ArgumentException();
+
+			var idx = offset;
+			var end = offset + length;
+			for (; idx < end; idx++)
+			{
+				var b = dataBuffer[idx];
+				var lo = HexMap[b & 0xf];
+				var hi = HexMap[b >> 4];
+				outputBuffer[outputOffset++] = hi;
+				outputBuffer[outputOffset++] = lo;
+			}
+			return length * 2;
+		}
+	}
+
+	sealed class CharArrayFieldWriter : FieldWriter
+	{
+		char[] dataBuffer = new char[128];
+
+		public override void WriteField(Context context, int ordinal)
+		{
+			var w = context.xw;
+			w.Write("<c t=\"str\"><v>");
+			var idx = 0;
+			int len;
+			var reader = context.dr;
+			while ((len = (int)reader.GetChars(ordinal, idx, dataBuffer, 0, dataBuffer.Length)) != 0)
+			{
+				w.Write(dataBuffer, 0, len);
+				idx += len;
+			}
 
 			w.Write("</v></c>");
 		}
