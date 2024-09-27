5018fcfb513a85085d775f19f66a09209a6347aa
MarkPflug <MarkPflug@users.noreply.github.com>
2024-09-27 15:59:38 -0700

    Implement hidden row handling for .xlsb files.

--- source/Sylvan.Data.Excel/Xlsb/XlsbWorkbookReader.cs
+++ source/Sylvan.Data.Excel/Xlsb/XlsbWorkbookReader.cs
@@ -27,6 +27,13 @@ sealed class XlsbWorkbookReader : ExcelDataReader
 
 	public override ExcelWorkbookType WorkbookType => ExcelWorkbookType.ExcelXml;
 
+	public override bool IsRowHidden {
+		get
+		{
+			return this.isRowHidden && this.rowIndex == this.parsedRowIndex;
+		}
+	}
+
 	public override void Close()
 	{
 		this.sheetStream?.Close();
@@ -287,6 +294,11 @@ sealed class XlsbWorkbookReader : ExcelDataReader
 			while (true)
 			{
 				var c = ParseRowValues();
+				if (this.readHiddenRows == false && this.IsRowHidden)
+				{
+					rowIndex++;
+					continue;
+				}
 				if (c < 0)
 				{
 					this.rowFieldCount = 0;
@@ -345,6 +357,8 @@ sealed class XlsbWorkbookReader : ExcelDataReader
 
 		var rowIdx = reader.GetInt32(0);
 		var ifx = reader.GetInt32(4);
+		var flags = reader.GetByte(11);
+		this.isRowHidden = (flags & 0x10) != 0;
 
 		reader.NextRecord();
 		int count = 0;
