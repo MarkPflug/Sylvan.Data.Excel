f431e23664f8ce43f2974708955fd2b1719483a0
MarkPflug <MarkPflug@users.noreply.github.com>
2025-09-19 12:39:08 -0700

    Disable dispose test which was causing contention with other tests.

--- source/Sylvan.Data.Excel.Tests/ExcelDataReaderTests.cs
+++ source/Sylvan.Data.Excel.Tests/ExcelDataReaderTests.cs
@@ -740,7 +740,7 @@ public partial class XlsxTests
 		}
 	}
 
-	[Fact]
+	[Fact(Skip = "This test causes problems with other tests using the Numbers file.")]
 	public void Dispose()
 	{
 		var file = GetFile("Numbers");
@@ -754,6 +754,7 @@ public partial class XlsxTests
 			}
 		}
 		// implied assertion that we are able to open the file, indicating that it was properly disposed.
+		// TODO: we can't exclusively lock this file here, because other concurrent tests might need to read it.
 		var s = File.Open(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
 		s.Dispose();
 	}
